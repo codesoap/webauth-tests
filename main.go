e56150f343d64436b0a9f46f75fd27d90b630798
codesoap <codesoap@mailbox.org>
2019-10-10 21:04:50 +0200

    Put everything into main.go

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 var jwt_secret = []byte("my secret secret")
@@ -83,4 +87,27 @@ func serveProfile(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Not authorized\n")
 	}
 	return
+}
+
+func storeSecret(username, password string) {
+	os.Mkdir("data", 0600)
+	f, _ := os.OpenFile("data/secrets", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), 10)
+	f.WriteString(username + ":" + string(hash) + "\n")
+}
+
+func passwordIsOk(username, password string) bool {
+	hash := getSecret(username)
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
+}
+
+func getSecret(username string) string {
+	f, _ := os.OpenFile("data/secrets", os.O_RDONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		return strings.SplitAfterN(scanner.Text(), ":", 2)[1]
+	}
+	return ""
 }
